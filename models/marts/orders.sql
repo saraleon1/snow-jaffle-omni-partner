40adebf4004e111781ace403b9628262fe273e7b
Sara Leon <sara@omni.co>
2025-12-16 09:41:41 +0000

    push to remote

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -49,8 +49,8 @@ compute_booleans as (
         order_items_summary.count_food_items,
         order_items_summary.count_drink_items,
         order_items_summary.count_order_items,
-        order_items_summary.count_food_items > 0 as is_food_order,
-        order_items_summary.count_drink_items > 0 as is_drink_order
+        -- order_items_summary.count_food_items > 0 as is_food_order,
+        -- order_items_summary.count_drink_items > 0 as is_drink_order
 
     from orders
 
